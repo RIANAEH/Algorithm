88858fab767f8c62cc9f9912282fa6b1ceecd19a
ririhan <45311765+RIANAEH@users.noreply.github.com>
2021-10-01 16:24:15 +0900

    정수 삼각형

---
+++ Programmers/Dynamic Programming/43105.py
@@ -0,0 +1,16 @@
+def solution(triangle):
+    answer = 0
+
+    # dp 테이블을 생성한다. 
+    dp = [[triangle[0][0]]]
+    for i in range(1, len(triangle)):
+        dp.append([0 for _ in range(i + 1)])
+
+    # 경로를 탐색하며 더 큰 합으로 변경한다. 
+    for i in range(len(triangle) - 1):
+        for j in range(i + 1):
+            dp[i + 1][j] = max(dp[i + 1][j], dp[i][j] + triangle[i + 1][j])
+            dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j] + triangle[i + 1][j + 1])
+
+    # 최댓값을 return
+    return max(dp[len(triangle) - 1])
